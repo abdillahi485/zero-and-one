d29b8e2bb47bbf62261bad62bf1305a189fc77e7
Abdillahi485 <daakircadde143@gmail.com>
2023-12-10 04:33:13 +0300

    finished

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Good Luck 💪🏾
+function ZeroAndOne(val){
+   
+    
+   
+  for(let i=0;i<val.length;i++){
+    let zero="Zero"
+    let one="One"
+    if(val[i] === one){
+        one=1;
+        console.log(one)
+    }else if(val[i] === zero){
+        zero=0
+        console.log(zero)
+    }
+  }
+}
+
+ZeroAndOne(["One","zero","Zero","One"])
+// ZeroAndOne(["Zero", "ONE", "one", "Zero"])
