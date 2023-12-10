c93ea40d917e267388de46930e8dfa1b666f0a04
Abdillahi485 <daakircadde143@gmail.com>
2023-12-10 05:38:10 +0300

    finidhed zero-and-one

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 // Good Luck 💪🏾
-function ZeroAndOne(val){
+// function ZeroAndOne(val){
    
     
    
-  for(let i=0;i<val.length;i++){
-    let zero="Zero"
-    let one="One"
-    if(val[i] === one){
-        one=1;
-        console.log(one)
-    }else if(val[i] === zero){
-        zero=0
-        console.log(zero)
+//   for(let i=0;i<val.length;i++){
+//     let zero="Zero"
+//     let one="One"
+//     if(val[i] === one){
+//         one=1;
+//         console.log(one)
+//     }else if(val[i] === zero){
+//         zero=0
+//         console.log(zero)
+//     }
+//   }
+// }
+
+function ZeroAndOne(words) {
+  const convertedArray = [];
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+
+    if (word === 'Zero') {
+      convertedArray.push(0);
+    } else if (word === 'One') {
+      convertedArray.push(1);
     }
   }
-}
 
-ZeroAndOne(["One","zero","Zero","One"])
-// ZeroAndOne(["Zero", "ONE", "one", "Zero"])
+  return convertedArray;
+}
+console.log(ZeroAndOne(["One","zero","Zero","One"]))
+// // ZeroAndOne(["Zero", "ONE", "one", "Zero"])
